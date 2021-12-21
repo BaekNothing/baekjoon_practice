5a6a6074ef5799d165c0cc1411ed763d3f96afe4
BaekNothing <47516849+BaekNothing@users.noreply.github.com>
2021-12-22 02:39:25 +0900

    terrot wip
    정밀도 문제때문인것 같은데, 내일 다시 보기

--- basic_math/1002/main.cpp
+++ basic_math/1002/main.cpp
@@ -7,26 +7,43 @@ int main(void){
     int xone, yone, xtwo, ytwo, radone, radtwo;
     int xpow, ypow, distance;
 
+    int radsum;
+
     std::cin >> counter;
 
     while(counter > 0)
     {
         distance = 0;
-        std::cin >> xone >> yone >> xtwo >> ytwo >> radone >> radtwo;
-        xpow = xone - xtwo > 0 ? xone - xtwo : xtwo - xone;
-        ypow = yone - ytwo > 0 ? yone - ytwo : ytwo - yone;
-        distance = sqrt(xpow * xpow + ypow * ypow);
-
-        if(radone + radtwo == distance)
-            std::cout << "1" << std::endl;
-        else if (radone + radtwo < distance)
-            std::cout << "0" << std::endl;
-        else if (radone > radtwo + distance)
-            std::cout << "0" << std::endl;
-        else if (radtwo > radone + distance)
-            std::cout << "0" << std::endl;
+        std::cin >> xone >> yone >> radone >> xtwo >> ytwo >> radtwo;
+        xpow = xone - xtwo;
+        if(xpow < 0)
+            xpow *= -1;
+        ypow = yone - ytwo;
+        if(ypow < 0)
+            ypow *= -1;
+            
+        radsum = radone + radtwo;
+        distance = xpow * xpow + ypow * ypow;
+        if(distance != 0)
+        {
+            if(radsum * radsum == distance)
+                std::cout << 1 << std::endl;
+            else if (radsum * radsum < distance)
+                std::cout << 0 << std::endl;
+            else if (radone > radtwo + distance)
+                std::cout << 0 << std::endl;
+            else if (radtwo > radone + distance)
+                std::cout << 0 << std::endl;
+            else
+                std::cout << 2 << std::endl;
+        }
         else
-            std::cout << "2" << std::endl;
+        {
+            if(radone == radtwo)
+                std::cout << -1 << std::endl;
+            else
+                std::cout << 0 << std::endl;
+        }
         counter--;
     }
     return (0);
