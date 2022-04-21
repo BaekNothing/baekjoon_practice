ac1359607f97fcc175a4b9de70370f4fc0d99548
Baeknothing <baeknothing@gmail.com>
2022-04-21 19:00:57 +0900

    trynqueen

--- notClassification/9663/main.cpp
+++ notClassification/9663/main.cpp
@@ -3,6 +3,7 @@
 
 bool compUp(int src, int dest, int distance)
 {
+	std::cout << src << dest << distance << "\n";
 	if (dest - distance == src)
 		return true;
 	return false;
@@ -10,42 +11,35 @@ bool compUp(int src, int dest, int distance)
 
 bool compDown(int src, int dest, int distance)
 {
+	std::cout << src << dest << distance << "\n";
 	if (dest + distance == src)
 		return true;
 	return false;
 }
 
-int findQueen(int index, int pos, int ary[15], int max)
+void printAry(int ary[16])
+{
+	for (int i = 1; i < 16; i ++)
+		std::cout << ary[i];
+	std::cout << "\n";
+}
+
+int findQueen(int index, int pos, int ary[16], int max)
 {
 	int result = 1;
 	ary[index] = pos;
 
-	//std::cout << index << ", " << pos << ", " << max << "\n" ;
-	for (int i = 0; i < 15; i ++)
-	 	std::cout << ary[i];
-	std::cout << "\n";
+	printAry(ary);
 
-	for (int i = 0; i < index; i++)
-	{
+	for (int i = 1; i < index; i++)
 		if (compUp(ary[index], ary[i], index - i) ||
 				compDown(ary[index], ary[i], index - i) ||
 			  ary[index] == ary[i])
-		{
-				std::cout << ary[index] << " " << ary[i] << "\n";
-				if(pos <= max)
-				{
-					std::cout << "true" << "\n";
-					result += findQueen(index, pos + 1, ary, max);
-				}
-				else
-				{
-					std::cout << "false" << "\n";
 					return(0);
-				}
-		}
-	}
 	if(index < max)
 		result += findQueen(index + 1, 1, ary, max);
+	if(pos < max)
+		result += findQueen(index, pos + 1, ary, max);
 	if (index == max)
 		return (result);
 	return (0);
@@ -54,14 +48,14 @@ int findQueen(int index, int pos, int ary[15], int max)
 int main(void)
 {
 
-	int ary[15];
-	for (int i = 0; i < 15; i++)
+	int ary[16];
+	for (int i = 0; i < 16; i++)
 		ary[i] = 0;
 	int max = 0;
 	int result = 0;
 
 	std::cin >> max;
-	result = findQueen(0, 1, ary, max - 1);
+	result = findQueen(1, 1, ary, max);
 	std::cout << result;
 
   return (0);
