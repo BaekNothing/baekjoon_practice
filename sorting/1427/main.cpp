a5448d9d532163df4b9e3d8a3a9b86dfd64b02a8
Baeknothing <baeknothing@gmail.com>
2022-04-26 11:24:11 +0900

    1427 각 자리수가 0~9라는 것을 이용해 해결.

--- sorting/1427/main.cpp
+++ sorting/1427/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <cstdio>
 
+using namespace std;
+
 int main()
 {
+  long long input;
+  int ary[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+  cin >> input;
+  while(input != 0)
+  {
+    ary[input % 10]++;
+    input /= 10;
+  }
+
+  for(int i = 9; i >= 0 ; i--)
+    while(ary[i]-- > 0)
+      cout << i;
   return 0;
 }
