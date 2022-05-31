c6e3a497e8f8b5f8b39cd2680697a008669d58fe
Baeknothing <baeknothing@gmail.com>
2022-05-31 12:48:27 +0900

    참고용 사이트 추가

---
+++ notClassification/python/0530Test.py
@@ -0,0 +1,5 @@
+url = "https://colab.research.google.com/github/haven-jeon/KoGPT2-chatbot/blob/master/KoGPT2_chatbot_pytorch.ipynb"
+url += "https://github.com/haven-jeon/KoGPT2-chatbot"
+url += "https://github.com/SKT-AI/KoGPT2"
+url += "https://vision-oasis.tistory.com/7?category=900851"
+
