ccfc128f70fb9c2800020e2eeda64153458cf5f4
Baeknothing <baeknothing@gmail.com>
2022-07-05 19:12:41 +0900

    optimize

--- number/2981/main.py
+++ number/2981/main.py
@@ -1,16 +1,16 @@
 import math
 
-from numpy import append
-
 length = int(input())
 ary = []
 for i in range(length) : 
     ary.append(int(input()))
-ary.sort()
 
 reary = []
 for i in range(length - 1) :
-    reary.append(ary[i + 1] - ary[i])
+    if ary[i] < ary[i + 1] :
+        reary.append(ary[i + 1] - ary[i])
+    else :
+        reary.append(ary[i] - ary[i + 1])
 
 GCD = reary[0]
 for i in range(1, reary.__len__()) :
@@ -19,6 +19,4 @@ for i in range(1, reary.__len__()) :
 result = []
 for i in range(2, GCD + 1) :
     if GCD % i == 0 :
-        result.append(i)
-
-print(*result)
+        print(i, end = ' ')
