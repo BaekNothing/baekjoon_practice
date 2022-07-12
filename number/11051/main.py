897188413642254cce260fb81cb0fefc4fd97569
BaekNothing <baeknothing@gmail.com>
2022-07-13 07:45:13 +0900

    Update main.py

--- number/11051/main.py
+++ number/11051/main.py
@@ -2,10 +2,12 @@ n, k = input().split()
 n = int(n)
 k = int(k)
 
-def pactorial(input) :
+
+def pactorial(input):
     result = 1
-    for  i in range(1, int(input + 1)) :
+    for i in range(1, int(input + 1)):
         result *= i
     return int(result)
+
 
 print(int(pactorial(n) // ((pactorial(n - k) * pactorial(k))) % 10007))
