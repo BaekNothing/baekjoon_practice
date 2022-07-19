629cdf04e65ef7c230253c4743d239f4df364459
BaekNothing <baeknothing@gmail.com>
2022-07-20 00:54:50 +0900

    dynamic

--- Dynamic programming/1149/main.py
+++ Dynamic programming/1149/main.py
@@ -2,33 +2,21 @@ length = int(input())
 numberAry = [[] for _ in range(length)]
 for i in range(length) :
     numberAry[i] = list(map(int, input().split()))
-sumAry = [[[0,0], [0,0], [0,0]] for _ in range(length)]
-sumAry[0][0] = [0, numberAry[0][0]]
-sumAry[0][1] = [1, numberAry[0][1]]
-sumAry[0][2] = [2, numberAry[0][2]]
+sumAry = [[0, 0, 0] for _ in range(length + 1)]
 
-for i in range(1, length) : 
+def findSmaller(i, j) : 
+    left = (j + 1) % 3
+    right = (j + 2) % 3
+    if sumAry[i][left] < sumAry[i][right] :
+        return left
+    else :
+        return right
+
+for i in range(1, length + 1) : 
     for j in range(3) :
-        index = sumAry[i][j][0]
-        if (numberAry[i][(index + 1) % 3] < numberAry[i][(index + 2) % 3]) :
-            newIndex = (index + 1) % 3
-            newValue = numberAry[i][(j + 1) % 3]
-        elif (numberAry[i][(index + 1) % 3] < numberAry[i][(index + 2) % 3]) :
-            newIndex = (index + 2) % 3
-            newValue = numberAry[i][(index + 2) % 3]
-        elif i + 1 < length :
-            if (numberAry[i + 1][(index + 1) % 3] < numberAry[i + 1][(index + 2) % 3]) :
-                newIndex = (index + 1) % 3
-                newValue = numberAry[i][(j + 1) % 3]
-            elif (numberAry[i + 1][(index + 1) % 3] < numberAry[i + 1][(index + 2) % 3]) :
-                newIndex = (index + 2) % 3
-                newValue = numberAry[i][(index + 2) % 3]
-        else :
-            newIndex = (index + 1) % 3
-            newValue = numberAry[i][(index + 1) % 3]
+        index = findSmaller(i - 1, j)
+        value = sumAry[i - 1][index]
+        sumAry[i][j] = numberAry[i - 1][j] + value
 
-        sumAry[i][j] = [newIndex, sumAry[i - 1][j][1] + newValue]
-
-print(sumAry[length - 1])
-print(min(sumAry[length - 1][0], sumAry[length - 1][1], sumAry[length - 1][2]))
+print(min(sumAry[length][0], sumAry[length][1], sumAry[length][2]))
 
