79a87cb49aedb3debe71c42a8bf4e92e11ccb7e4
Baeknothing <baeknothing@gmail.com>
2022-07-22 19:01:26 +0900

    add EveryThingIsObject!!

--- explain/EveryThingIsObject.py
+++ explain/EveryThingIsObject.py
@@ -3,3 +3,13 @@ print('\033[95m',type(print),'\033[0m')
 input = input()
 print(dir(input))
 print('\033[95m',type(input),'\033[0m')
+
+def objectIsObejct() :
+    print("Hello?")
+
+objectIsObejct()
+print(objectIsObejct)
+newFunction = objectIsObejct
+objectIsObejct = "Hello"
+print(objectIsObejct)
+newFunction()
