6dd48afac3220057653105e2942ae476793acce9
Baeknothing <baeknothing@gmail.com>
2022-08-03 13:47:57 +0900

    uyu aya

---
+++ notClassification/17363/main.py
@@ -0,0 +1,26 @@
+﻿charMap = {
+    '.' : '.',
+    'O' : 'O',
+    '-' : '|',
+    '|' : '-',
+    '/' : '\\',
+    '\\' : '/',
+    '^' : '<',
+    '<' : 'v',
+    'v' : '>',
+    '>' : '^'
+}
+
+height, width = map(int, input().split())
+
+oldLine = []
+for i in range(height):
+    inputCharList = list(input())
+    oldLine.append(inputCharList)
+
+rotatedLine = [['' for i in range(height)] for j in range(width)]
+for i in range(height):
+    for j in range(width):
+        rotatedLine[j][i] = charMap[oldLine[i][j]]
+rotatedLine.reverse()
+print ('\n'.join(''.join(row) for row in rotatedLine))
